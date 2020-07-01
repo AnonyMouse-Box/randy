c1bb35ce6f13452ff0eac175dc97266e7825ff28
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-07-01 23:56:59 +0100

    Create coin.py

---
+++ coin.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+
+import random
+
+class coin:
+    def __init__(self):
+        self.weightHeads = 1
+        self.weightTails = 1
+        self.clumsy = 0
+        return;
+
+    def __setHeads(self, weight):
+        self.weightHeads = weight
+        return;
+
+    def __setTails(self, weight):
+        self.weightTails = weight
+        return;
+
+    def __setClumsy(self, weight):
+        self.clumsy = weight
+        return;
+
+    def __flipCoin(self):
+        pseudobool = random.randrange(self.weightHeads + self.weightTails + self.clumsy + 1)
+        if pseudobool <= self.weightHeads:
+            result = "heads"
+        elif pseudobool <= self.weightHeads + self.weightTails:
+            result = "tails"
+        else:
+            result = "lost"
+        return result;
+
+    def flipCoins(self, quantity):
+        coins = [self.__flipCoin() for a in range(quantity)]
+        return coins;
+
+    def weightCoin(self, heads, tails, clumsy):
+        self.__setHeads(heads)
+        self.__setTails(tails)
+        self.__setClumsy(clumsy)
+        return;
