a36c1e41a3c8b2c4c616a91a9523281a8b03ab5b
C.Monkey <36849880+AnonyMouse-Box@users.noreply.github.com>
2020-07-01 23:55:42 +0100

    Create randy.py

---
+++ randy.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+import random
+import coin
+import die
+
+class Randy(object):
+    '''
+    This bot generates random coins and dice with biases
+    '''
+
+    def usage(self):
+        return '''This bot generates random coins and dice with biases'''
+
+    def handle_message(self, message, bot_handler):
+        results = ""
+
+        if message['content'] == "coin":
+            a = coin.coin()
+            results = a.flipCoins(1)
+        elif message['content'] == "dice":
+            a = die.createSimpleDie(6)
+            results = a.rollDice(1)
+
+        if message['content'] == "help":
+            content = "type `coin` for a coin flip or `dice` for a dice roll"
+        elif results == "":
+            content = "yes?"
+        else:
+            content = ""
+            for item in results:
+                content += ", " + str(item)
+            content = content[2:]
+
+        bot_handler.send_reply(message, content)
+        return;
+
+handler_class = Randy
