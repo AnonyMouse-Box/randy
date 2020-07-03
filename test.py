767501202a4b5b832a82f6d7c94c1186a1a63e9e
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2020-07-03 13:48:02 +0100

    Add new file

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin.env python3
+
+
+class test:
+    def __init__(self, value):
+        self.value = value
+        return;
+
+    def ifExists(self, default):
+        if self.value == "":
+            self.value = default
+        else:
+            self.value = int(self.value)
+        return;
