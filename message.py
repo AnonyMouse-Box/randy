fc68ea89edd7ae3907aeb6f1d570985897613256
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2020-07-05 12:06:29 +0100

    Add new file

---
+++ message.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+import re
+
+class message(object):
+    def __init__(self):
+        self.__text = False
+        self.__match = False
+        self.__request = False
+        return;
+
+    def set_text(self, text):
+        self.__text = text.strip()
+        return;
+
+    def get_request(self):
+        if self.__request == False:
+            raise TypeError("validate input first!")
+        return self.__request;
+
+    def validate(self):
+        if self.__text == False:
+            raise TypeError("set text first!")
+        elif self.__text == "":
+            raise TypeError("empty string!")
+        self.__match = re.match("^([0-9]{1,})$", self.__text)
+        if self.__match == None:
+            raise TypeError("not an integer")
+        self.__request = int(self.__match.group(1))
+        return;
+
+def get_message(name):
+    name = message()
+    name.set_text(input("Enter N value: "))
+    print("\n")
+    try:
+        name.validate()
+        return name.get_request();
+    except TypeError as error:
+        print("Error: invalid input, {0}".format(error))
+
+n = get_message("text")
+print(n)
