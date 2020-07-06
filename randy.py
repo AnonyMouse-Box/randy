85f3d8905fd0a1081b7fd864909f22b34855aadb
AnonyMouse <3-AnonyMouse@noreply.downbox.co.uk>
2020-07-06 16:22:25 +0100

    Update randy.py

--- randy.py
+++ randy.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 
-import re
-from . import message
+from . import expression
 from . import coin
 from . import die
 from . import test
@@ -17,6 +16,8 @@ class Randy(object):
 
     def handle_message(self, message, bot_handler):
         content = False
+        text = message['content']
+
         # detects help message
         if "help" in text:
             content =   """
@@ -28,7 +29,7 @@ Coin; @randy {<times> }c{oins weight <heads>:<tails>:<clumsy>}
 Examples; long:- `@randy 20 coin weight 20:10:30`, short:- `@randy 20cw2:1:3`
 ------------
 Dice; @randy {<times> }d{ice <faces>,<stop>,<step> weight <number> <load>:<rest>:<clumsy> <operand>}
-Examples; long:- `@randy 20 dice 16,24,2 weight 15 20:10:30 +89`, short:- `@randy 20d16,24,2w15 20:10:30+89`
+Examples; long:- `@randy 20 dice 16,24,2 weight 16 20:10:30 +89`, short:- `@randy 20d16,24,2w15 20:10:30+89`
 ------------
 Times - the number of times the action will be performed, with dice all results will be automatically accumulated and a final result given.
 Coin - the command used to define a coin flip.
@@ -55,7 +56,11 @@ Operand - this defines an operand  `+`, `-`, `/`, or `*` that you can perform up
             content = "yes?"
         
         else:
-            matches = get_message("text", message['content'])
+            print(text)
+            command = expression.expression()
+            matches = command.get_message(text)
+            if matches == None:
+                matches = []
 
             # detects coin input
             if len(matches) == 4:
@@ -63,9 +68,10 @@ Operand - this defines an operand  `+`, `-`, `/`, or `*` that you can perform up
                 heads = test.test(matches[1])
                 tails = test.test(matches[2])
                 clumsy = test.test(matches[3])
+                operand = ""
                 item = coin.coin()
                 print("times = {0}\nheads = {1}\ntails = {2}\nclumsy = {3}".format(times.value, heads.value, tails.value, clumsy.value))
-                results = a.parseCoin(times, heads, tails, clumsy)
+                results = item.parseCoin(times, heads, tails, clumsy)
                 slug = "You flipped a coin " + str(times.value) +  " times, the result was:\n"
 
             # detects dice input
@@ -82,45 +88,50 @@ Operand - this defines an operand  `+`, `-`, `/`, or `*` that you can perform up
                 value = test.test(matches[9])
                 if stop.value == "":
                     faces.ifExists(6)
-                    a = die.createSimpleDie(faces.value)
+                    item = die.createSimpleDie(faces.value)
                 else:
                     faces.ifExists(1)
                     stop.ifExists(6)
                     step.ifExists(1)
-                    a = die.die(faces.value, stop.value, step.value)
-                print("times = {0}\nfaces = {1}\nstop = {2}\nstep = {3}\nnumber = {4}\nload = {5}\nrest = {6}\nclumsy = {7}\noperand = {8}\nvalue = {9}\n".format(times.value, faces.value, stop.value, step.value, number.value, load.value, rest.value, clumsy.value, operand, factor.value))
-                results = a.parseDice(times, number, load, rest, clumsy)
-                slug = "You rolled a D" + str(a.faces) + " " + str(times.value) + " times, the result was:\n"
+                    item = die.die(faces.value, stop.value, step.value)
+                print("times = {0}\nfaces = {1}\nstop = {2}\nstep = {3}\nnumber = {4}\nload = {5}\nrest = {6}\nclumsy = {7}\noperand = {8}\nvalue = {9}\n".format(times.value, faces.value, stop.value, step.value, number.value, load.value, rest.value, clumsy.value, operand, value.value))
+                results = item.parseDice(times, number, load, rest, clumsy)
+                slug = "You rolled a D" + str(item.faces) + " " + str(times.value) + " times, the result was:\n"
 
             # catches any other kind of input
             else:
                 content = "I'm sorry what was that? I don't understand.\nType `help` to see the commands I know."
 
             # parses output in message friendly way
-            if content = False:
+            if content == False:
                 content = ""
                 acc = 0
                 for item in results:
-                    acc += item
+                    if isinstance(item, int):
+                        acc += item
                     content += ", " + str(item)
                 content = content[2:]
                 content = slug + content
-                if operand == "" and len(matches) == 10:
+                if operand is None and len(matches) == 10:
                     content += " = " + str(acc)
-                elif operand != 0:
+                elif operand is not None and len(operand) != 0:
                     value.ifExists(1)
-                    if operand == "+":
-                        acc += value.value
-                    elif operand == "-":
-                        acc -= value.value
-                    elif operand == "*":
-                        acc *= value.value
-                    elif operand == "/":
-                        acc /= value.value
-                    content += " " operand + " " + str(value.value) + " = " + str(acc)
+                    try:
+                        if operand == "+":
+                            acc += value.value
+                        elif operand == "-":
+                            acc -= value.value
+                        elif operand == "*":
+                            acc *= value.value
+                        elif operand == "/":
+                            acc /= value.value
+                    except ZeroDivisionError:
+                            content = "divide by zero? how dumb do you think I am?!"
+                    else:
+                        content += " " + operand + " " + str(value.value) + " = " + str(acc)
 
-        # sends completed message and ends
-        bot_handler.send_reply(message, content)
-        return;
+            # sends completed message and ends
+            bot_handler.send_reply(message, content)
+            return;
 
 handler_class = Randy
